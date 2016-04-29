97ab412262686aa36762f2270b34c31170cfd209
Márton Makrai <makrai.nlp@gmail.com>
2016-04-29 17:41:02 +0200

    rows, cols bug fixed

--- coclust_tade.py
+++ coclust_tade.py
@@ -11,93 +11,116 @@ from sklearn.manifold import TSNE
 from sklearn.cluster.bicluster import SpectralCoclustering, SpectralBiclustering
 from matplotlib import pyplot as plt 
 from matplotlib.colors import LogNorm
+from matplotlib import ticker
 
 
 class TadeClustering(): 
-    # a sort-beli cutoffna1l bejo2n csupa nulla sor
-    # TODO  mut_info?
-    def __init__(self):
-        self.input_filen = '/mnt/store/hlt/Language/Hungarian/Dic/tade_cutoff_with-aux.tsv'
+    def __init__(self, short=False, transpose=False, load=False): 
+        self.input_filen = '/mnt/store/hlt/Language/Hungarian/Dic/tade/tade_cutoff_with-aux.tsv'
         self.mx_filen = 'tade_mx.npz'
+        self.short = short
+        self.transpose=transpose
+        self.load=load
 
-    def main(self, print_cutoff=False, transpose=False):
+    def main(self, print_cutoff=False, log_mx=False):
+        # TODO mut_info and cocluster are incompatible
+        # TODO ACC-ot ku2lo2n kezelni?
         if print_cutoff:
             self.read_frame_ent()
             self.print_freq_frame()
         else: 
             self.read_tade_dict()
-            if transpose:
+            if self.transpose:
                 logging.info('Transposing')
                 self.mx = self.mx.T
-            #self.mut_info()
             self.sort_lines()#cut_off=False)
-            self.cocluster()
-            #self.dim_reduce()#apply_tsne=False)
-            #np.savez(self.mx_filen, self.mx, self.cases, self.verbs)
-            self.plot_tade(self.mx, transpose)#, savefig=True) 
+            #self.mut_info(log_mx)
+            #self.cocluster()
+            self.dim_reduce()#apply_tsne=False)
+            if self.load:
+                np.savez(self.mx_filen, self.mx, self.cols, self.rows)
+            self.plot_tade(log_mx)
 
-    def read_tade_dict(self, collate_aux=True, short=False, load=False):
+    def read_tade_dict(self, collate_aux=True, row_is_prev=False):
         logging.info("Reading Tade to a dictionary..")
         # TODO DEBUG load
-        if load and isfile(self.mx_filen):
+        if self.load and isfile(self.mx_filen):
             logging.info('Loading mx..')
-            self.mx, self.cases, self.verbs = np.load(self.mx_filen)
+            self.mx, self.cols, self.rows = np.load(self.mx_filen)
             logging.debug(type(self.mx))
             return
         with open(self.input_filen) as tade_f:
-            tade_d = defaultdict(int)
+            self.tade_dict = defaultdict(int)
             for line in tade_f:
                 verb, frame, freq, vfreq, _ = line.split()
+                frame = frame.upper()
                 freq = int(freq)
                 if collate_aux: 
                     verb = verb.split('_')[-1]
-                if short:
-                    for cas in frame.split('_'):
-                        tade_d[verb, cas] += freq
+                if row_is_prev:
+                    if '+' in verb: 
+                        prev, verb = verb.split('+', 1)
+                        if '+' in verb:
+                            continue 
+                        self.book_clause(prev, frame, freq)
                 else:
-                    tade_d[verb, frame] += freq 
-        self.get_tade_mx(tade_d)
+                    self.book_clause(verb, frame, freq)
+        self.dict_to_mx()
 
-    def get_tade_mx(self, tade_d):
-        self.verbs, self.cases = zip(*tade_d.keys())
-        case_i = {cas: i for i, cas in enumerate(set(self.cases))}
-        verb_i = {vrb: i for i, vrb in enumerate(set(self.verbs))}
-        #self.mx =  lil_matrix((len(verb_i), len(case_i)))#, 'int')
-        self.mx =  np.zeros((len(verb_i), len(case_i)))
+    def book_clause(self, verb, frame, freq, type_freq=False):
+        if type_freq:
+            freq = 1
+        if self.short:
+            for cas in frame.split('_'):
+                self.tade_dict[verb, cas] += freq
+        else:
+            self.tade_dict[verb, frame] += freq 
+
+    def dict_to_mx(self, sparse=False):
+        self.rows, self.cols = [np.array(list(set(tuple_)))
+                                for tuple_ in zip(*self.tade_dict.keys())]
+        logging.debug(self.rows)
+        logging.debug(self.cols[:1])
+        case_i = {cas: i for i, cas in enumerate(self.cols)}
+        verb_i = {vrb: i for i, vrb in enumerate(self.rows)}
+        if sparse:
+            self.mx =  lil_matrix((len(verb_i), len(case_i)))#, 'int')
+        else:
+            self.mx =  np.zeros((len(verb_i), len(case_i)))
         logging.info(self.mx.shape)
-        for vrb, col in  tade_d.keys():
-            self.mx[verb_i[vrb], case_i[col]] = tade_d[vrb, col] 
+        for vrb, col in  self.tade_dict.keys():
+            self.mx[verb_i[vrb], case_i[col]] = self.tade_dict[vrb, col] 
 
-    def mut_info(self):
+    def mut_info(self, log_mx):
         logging.info('Computing mutual information..')
-        logging.debug('')
         #self.mx += 1
-        logging.debug('')
         n_token =  self.mx.sum()
-        logging.debug('')
         verb_freq = self.mx.sum(axis=1).reshape(-1,1)
-        logging.debug('')
-        case_freq = self.mx.sum(axis=0).reshape(1,-1)
+        cas_freq = self.mx.sum(axis=0).reshape(1,-1)
         logging.debug('Computing relative freq..')
         self.mx /= verb_freq
         logging.debug('Computing relative freq..')
-        self.mx /= case_freq
-        logging.debug('')
+        self.mx /= cas_freq
         self.mx *= n_token
-        logging.debug('')
-        self.mx = np.log(self.mx)
+        if log_mx:
+            self.mx = np.log(self.mx)
 
     def sort_lines(self, cut_off=True):
-        verb_argrank = (self.mx != 0).sum(axis=1).argsort()[::-1]
-        case_argrank = (self.mx != 0).sum(axis=0).argsort()[::-1]
-        if cut_off:
-            verb_argrank = verb_argrank[:10000]
-            case_argrank = case_argrank[:20]
+        verb_argrank = self.mx.sum(axis=1).argsort()[::-1]
+        cas_argrank = self.mx.sum(axis=0).argsort()[::-1]
+        if cut_off: 
+            if self.short: 
+                verb_argrank = verb_argrank[:1000]
+                cas_argrank = cas_argrank[:20]
+            else:
+                verb_argrank = verb_argrank[:2000]
         verb_argrank = verb_argrank.reshape(-1,1)
-        case_argrank = case_argrank.reshape(1,-1) 
-        self.verbs = np.array(self.verbs)[verb_argrank]
-        self.cases = np.array(self.cases)[case_argrank]
-        self.mx = self.mx[verb_argrank, case_argrank]
+        cas_argrank = cas_argrank.reshape(1,-1) 
+        self.mx = self.mx[verb_argrank, cas_argrank]
+        self.rows = self.rows[verb_argrank].reshape(-1)
+        self.cols = self.cols[cas_argrank].reshape(-1)
+        logging.debug(self.rows[:5])
+        logging.debug(self.cols[:4])
 
     def cocluster(self, blockdiag=False):
         logging.info('Co-clustering Tade..')
@@ -111,10 +134,12 @@ class TadeClustering():
         clusser.fit(self.mx)
         logging.info('Argsorting mx rows..')
         self.mx = self.mx[np.argsort(clusser.row_labels_)]
+        self.rows = self.rows[np.argsort(clusser.row_labels_)]
         logging.info('Argsorting mx cases..')
         self.mx = self.mx[:, np.argsort(clusser.column_labels_)]
+        self.cols = self.cols[np.argsort(clusser.column_labels_)]
 
-    def dim_reduce(self, apply_tsne=False):
+    def dim_reduce(self, apply_tsne=True):
         if self.mx.shape[1] > 400 or not apply_tsne:
             logging.info('PCA.. from {}'.format(self.mx.shape))
             pca = PCA(n_components=200 if apply_tsne else 2)
@@ -127,28 +152,57 @@ class TadeClustering():
         #   By default the gradient calculation algorithm uses Barnes-Hut
         #   approximation running in O(NlogN) time. method='exact' will run on
         #   the slower, but exact, algorithm in O(N^2) time
+        logging.debug(self.mx.shape)
 
-    def plot_tade(self, mx, transpose, savefig=False):
+    def plot_tade(self, log_mx, savefig=False, row_freq_ent=False, mut_info=False):
         logging.info('Plotting..')
         fig = plt.figure()
-        ax = fig.add_subplot(111)
+        self.ax = fig.add_subplot(111)
         if issparse(self.mx): 
-            ax.spy(self.mx[:200,:200], marker='.')
-            ax.set_aspect('auto')
+            self.ax.spy(self.mx[:200,:200], marker='.')
+            self.ax.set_aspect('auto')
+        elif row_freq_ent:
+            row_freq = self.mx.sum(axis=1).reshape(-1,1)
+            row_ent = np.apply_along_axis(entropy, 1, self.mx)
+            self.scatter(row_freq, row_ent)
         elif self.mx.shape[1] > 2:
-            cax = ax.matshow(self.mx, norm=LogNorm())
+            points, features = self.rows, self.cols
+            if self.transpose:
+                points, features = features, points
+            if log_mx:
+                cax = self.ax.matshow(self.mx)
+            else:
+                cax = self.ax.matshow(self.mx, norm=LogNorm())
+            label_limit = 150
+            if self.mx.shape[0] < label_limit:
+                self.ax.set_yticklabels([''] + self.rows.tolist())
+                self.ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+            if self.mx.shape[1] < label_limit:
+                self.ax.set_xticklabels([''] + self.cols.tolist())
+                self.ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+                locs, labels = plt.xticks()
+                plt.setp(labels, rotation=90)
             fig.colorbar(cax)
-            ax.set_aspect('auto')
+            self.ax.set_aspect('auto')
         else:
-            ax.scatter(*zip(*mx))
-            lab_col = self.cases if transpose else self.verbs
-            for label1, row in zip(lab_col.reshape(-1), self.mx[:100]):
-                ax.annotate(label1, xy=row, fontsize=10)
+            self.scatter(self.mx[:,0], self.mx[:,1])
         if savefig:
             logging.info('Saving fig..')
             plt.savefig('tade-mutinfo-tsne.pdf')
         else:
             plt.show() 
+
+    def scatter(self, xs, ys):
+        self.ax.scatter(xs, ys)
+        points, features = self.rows, self.cols
+        if self.transpose:
+            points, features = features, points
+        labels = np.array(['{} {}'.format(l, features[m]) for l, m in zip(
+            points, self.mx.argmax(axis=1))])
+        for label, row in zip(labels.reshape(-1), zip(xs, ys)):
+            break
+            self.ax.annotate(label, xy=row, fontsize=10)
+        #self.ax.set_xscale('log')
 
     def read_frame_ent(self):
         self.frame_ent = dict()
